b0546375c0a8b848aedb4c78fe911945e891f141
bhd436@myy.haaga-helia.fi <bhd436@myy.haaga-helia.fi>
2024-09-28 16:42:03 +0300

    kommentteja

--- oma.js
+++ oma.js
@@ -19,24 +19,29 @@ document.getElementById("vastaus").innerHTML = "<p>Tietoa ei pystytä hakemaan</
 })
 
 function kerro(data) {
+// määritellään muuttuja, johon tulostettava tieto kerätään
 var teksti = "";
+// otsikkotiedon hakeminen ja sijoittaminen h1-elementtiin
 teksti = "<h1>" + data.otsikko + "</h1>";
+// muiden tietojen hakeminen
 teksti = teksti + "<p>" + data.kuvaus + "</p>";
 teksti = teksti + "<p><img src='" + data.kuva + "' alt='kuva' ></p>";
 teksti = teksti + "<h3>Opintojakso: " + data.opintojakso.nimi + " " + data.opintojakso.tunnus + " " + data.opintojakso.opintopisteet + " op</h3>";
 
-
+// haetaan sisältö-taulukkomuotoista tietoa
 teksti = teksti + "<ul>";
 for(var i = 0; i < data.sisalto.length; i++) {
     teksti = teksti + "<li>" + data.sisalto[i] + "</li>";
 }
 teksti = teksti + "</ul>";
 
+// haetaan tekniikat-tietoa objektitaulukosta
 teksti = teksti + "<ul>";
 for(var i = 0; i < data.tekniikat.length; i++) {
     teksti = teksti + "<p>" + data.tekniikat[i].aihe + "</p>";
 }
 teksti = teksti + "</ul>";
 
+// teksti-muuttujan sisällön tulostus
 document.getElementById("vastaus").innerHTML = teksti;
 }
