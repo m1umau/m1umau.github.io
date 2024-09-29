7961caf72068aaa0ec48635eecc1e1fb5740f021
bhd436@myy.haaga-helia.fi <bhd436@myy.haaga-helia.fi>
2024-09-29 22:09:54 +0300

    tre

---
+++ oma3.js
@@ -0,0 +1,26 @@
+fetch('https://api.visittampere.com/api/v1/visittampere/event/published/all/?format=json&lang=fi')
+
+// Muunnetaan vastaus JSON muotoon
+.then(function (response) { return response.json();
+})
+// Käsitellään muunnettu (eli JSON muotoinen) vastaus
+.then(function (responseJson) {
+
+// Kutsutaan funktiota ja välitetään sille json-vastaus tapahtumat(responseJson)
+tapahtumat(responseJson);
+})
+
+// Jos tuli jokin virhe
+.catch(function (error) { document.getElementById("vastaus").innerHTML = "<p>Tietoa ei pystytä hakemaan </p>"+ error;
+})
+
+function tapahtumat(data) {
+    var teksti = "";
+    teksti = "<h1>Tampereella tapahtuu</h1>";
+    for (var i = 0; i < data.length; i++) {
+      teksti = teksti + "<h3>" + data[i].name + "</h3>";
+      teksti = teksti + "<p>" + data[i].description + "</p>";
+      teksti = teksti + "<p> <a href=" + data[i].url + ">" + data[i].url + "</a></p>";
+    }
+document.getElementById("vastaus").innerHTML = teksti;
+}
