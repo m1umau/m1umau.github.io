b873e206dd221bd7c5dfb36313c626161fbe5c06
bhd436@myy.haaga-helia.fi <bhd436@myy.haaga-helia.fi>
2024-09-29 16:51:30 +0300

    valmis

--- oma2.js
+++ oma2.js
@@ -25,17 +25,18 @@ var teksti = "";
 teksti = "<h1>" + data.nimi + "</h1>";
 // muiden tietojen hakeminen
 teksti = teksti + "<p>Osallistujien lukumäärä: " + data.osallistujia + "</p>";
-teksti = teksti + "<p><img src='" + data.kuva + "' alt='kuva' ></p>";
 teksti = teksti + "<p>Toteutuksen kesto: " + data.kesto + " viikkoa</p>";
 teksti = teksti + "<p>Alkamispäivä: " + data.ajankohta.alku + "</p>";
 teksti = teksti + "<p>Loppumispäivä: " + data.ajankohta.loppu + "</p>";
 
 // haetaan sisältö-taulukkomuotoista tietoa
-teksti = teksti + "<ul>Osallistujien nimet: ";
+teksti = teksti + "<p>Osallistujien nimet: ";
 for(var i = 0; i < data.nimet.length; i++) {
-    teksti = teksti + "<li>" + data.nimet[i].etunimi + data.nimet[i].sukunimi + "</li>";
+    teksti = teksti + "<li>" + data.nimet[i].etunimi + " " + data.nimet[i].sukunimi + "</li>";
 }
-teksti = teksti + "</ul>";
+teksti = teksti + "</p>";
+
+teksti = teksti + "<p><img src='" + data.kuva + "' alt='kuva' ></p>";
 
 // teksti-muuttujan sisällön tulostus
 document.getElementById("vastaus").innerHTML = teksti;
