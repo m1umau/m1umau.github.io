a7e9f137dc8b19ad07a258500cd599bf24e5116c
bhd436@myy.haaga-helia.fi <bhd436@myy.haaga-helia.fi>
2024-09-29 23:36:04 +0300

    ul

--- oma4.js
+++ oma4.js
@@ -21,14 +21,14 @@ var teksti = ""; // määritellään muuttuja, johon tulostettava tieto kerätä
 teksti = "<h1>Sää - siitä on aina jotain kerrottavaa</h1>";
 teksti = teksti + "<p>Millainen sää Helsingissä on? Entä Tampereella?</p>";
 
-teksti = teksti + "<li>kaupunki: " + data.name + "</li>";
+teksti = teksti + "<ul><li>kaupunki: " + data.name + "</li>";
 teksti = teksti + "<li>kuvaus: " + data.weather[0].description + "</li>";
 teksti = teksti + "<li>lämpötila: " + data.main.temp + "&degC</li>";
 teksti = teksti + "<li>tuulen nopeus: " + data.wind.speed + " m/s</li>";
 
 var kuva = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
 // tähän tulee muiden tietojen käsittely
-teksti = teksti + "<p><img src='" + kuva + "' alt='kuva' ></p>";
+teksti = teksti + "<p><img src='" + kuva + "' alt='kuva' ></p></ul>";
 // teksti-muuttujan sisällön tulostus
 document.getElementById("vastaus").innerHTML = teksti;
 }
