057d2e4769de9098e951d90f81cc06e7a7daba20
bhd436@myy.haaga-helia.fi <bhd436@myy.haaga-helia.fi>
2024-10-01 00:26:36 +0300

    tampere

--- oma5.js
+++ oma5.js
@@ -1,4 +1,4 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?lang=fi&q=helsinki&units=metric&APPID=314de1b8dabfbace7321a5ff474e27d1')
+fetch('https://api.openweathermap.org/data/2.5/weather?lang=fi&q=tampere&units=metric&APPID=314de1b8dabfbace7321a5ff474e27d1')
 // Muunnetaan vastaus JSON muotoon
 .then(function (response) { return response.json();
 })
@@ -11,16 +11,13 @@ saa(responseJson);
 })
 
 // Jos tuli jokin virhe
-.catch(function (error) { document.getElementById("vastaus").innerHTML = "<p>Tietoa ei pystytä hakemaan</p>";
+.catch(function (error) { document.getElementById("vastaus2").innerHTML = "<p>Tietoa ei pystytä hakemaan</p>";
 })
 
 function saa(data){
 var teksti = ""; // määritellään muuttuja, johon tulostettava tieto kerätään
 
 // otsikkotiedon hakeminen ja sijoittaminen h1-elementtiin
-teksti = "<h1>Sää - siitä on aina jotain kerrottavaa</h1>";
-teksti = teksti + "<p>Millainen sää Helsingissä on? Entä Tampereella?</p>";
-
 teksti = teksti + "<ul><li>kaupunki: " + data.name + "</li>";
 teksti = teksti + "<li>kuvaus: " + data.weather[0].description + "</li>";
 teksti = teksti + "<li>lämpötila: " + data.main.temp + "&degC</li>";
@@ -30,5 +27,5 @@ var kuva = 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
 // tähän tulee muiden tietojen käsittely
 teksti = teksti + "<p><img src='" + kuva + "' alt='kuva' ></p></ul>";
 // teksti-muuttujan sisällön tulostus
-document.getElementById("vastaus").innerHTML = teksti;
+document.getElementById("vastaus2").innerHTML = teksti;
 }
